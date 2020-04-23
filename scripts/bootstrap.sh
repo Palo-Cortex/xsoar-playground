8a24c4f63e1b1458ebc6d36a4153892d97511067
scottbrumley <scott@nsisecure.com>
2020-04-23 16:09:33 -0400

    Add licenses

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade -f -y --force-yes
-sudo apt-get update
+#sudo apt-get update
+#sudo apt-get upgrade -f -y --force-yes
+#sudo apt-get update
 
+### Add License
 cp /vagrant/demisto.lic /usr/local/demisto
 chown demisto:demisto /usr/local/demisto/demisto.lic
 chmod 644 /usr/local/demisto/demisto.lic
