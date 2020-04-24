270245bc83122c3740eadd0444d1b750558afd36
scottbrumley <scott@nsisecure.com>
2020-04-24 05:47:09 -0400

    Removed update and upgrades.  Will handle this in build project.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#sudo apt-get update
-#sudo apt-get upgrade -f -y --force-yes
-#sudo apt-get update
-
 ### Add License
 cp /vagrant/demisto.lic /usr/local/demisto
 chown demisto:demisto /usr/local/demisto/demisto.lic
